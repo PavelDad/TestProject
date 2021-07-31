75214f7e7da013daaa09355f66cdfead25070bf1
StadnikPavel <stadnikpavel86@gmail.com>
2021-07-31 15:28:24 +0600

    result

---
+++ TestTask/ReadOnlyStream.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace TestTask
+{
+    public class ReadOnlyStream : IReadOnlyStream
+    {
+        private FileStream _localStream;
+
+        /// <summary>
+        /// Конструктор класса. 
+        /// Т.к. происходит прямая работа с файлом, необходимо 
+        /// обеспечить ГАРАНТИРОВАННОЕ закрытие файла после окончания работы с таковым!
+        /// </summary>
+        /// <param name="fileFullPath">Полный путь до файла для чтения</param>
+        public ReadOnlyStream(string fileFullPath)
+        {
+            IsEof = false;
+
+            _localStream = new FileStream(fileFullPath, FileMode.Open);
+            // TODO : Заменить на создание реального стрима для чтения файла!
+            //_localStream = null;
+        }
+                
+        /// <summary>
+        /// Флаг окончания файла.
+        /// </summary>
+        public bool IsEof
+        {
+            // TODO : Заполнять данный флаг при достижении конца файла/стрима при чтении
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Ф-ция чтения следующего символа из потока.
+        /// Если произведена попытка прочитать символ после достижения конца файла, метод 
+        /// должен бросать соответствующее исключение
+        /// </summary>
+        /// <returns>Считанный символ.</returns>
+        public char ReadNextChar()
+        {
+            // TODO : Необходимо считать очередной символ из _localStream
+            if (_localStream.Position == _localStream.Length - 1)
+            {
+                IsEof = true;
+            }
+            int nextChar = _localStream.ReadByte();
+            if (nextChar == -1)
+            {
+                throw new EndOfStreamException("Достигнут конец файла.");
+            }
+            return (char)nextChar;
+        }
+
+        /// <summary>
+        /// Сбрасывает текущую позицию потока на начало.
+        /// </summary>
+        public void ResetPositionToStart()
+        {
+            if (_localStream == null)
+            {
+                IsEof = true;
+                return;
+            }
+
+            _localStream.Position = 0;
+            IsEof = false;
+        }
+
+        public void Dispose()
+        {
+            _localStream.Close();
+        }
+    }
+}
